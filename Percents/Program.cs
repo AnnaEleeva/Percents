1a06ce56be90b4768ecfeca08402ac060a005775
Anna <anna_eleeva@yahoo.com>
2021-02-24 18:51:28 +0500

    Добавьте файлы проекта.

---
+++ Percents/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Percents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s=Console.ReadLine();
+           
+            double d= Calculate(s);
+            Console.WriteLine(d);
+        }
+
+        public static double Calculate(string userInput)
+        {
+            
+            string[] c = userInput.Split(' ');
+         
+            double sum = double.Parse(c[0], CultureInfo.InvariantCulture);
+            double proc = double.Parse(c[1]);
+            double procInAMonth = (double)proc / 12;
+            var countMonth = int.Parse(c[2]);
+            return (sum * Math.Pow(1 + 0.01 * procInAMonth, countMonth));
+            //return 0.0;
+        }
+    }
+}
